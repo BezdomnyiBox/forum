491842289bfdd751936a4fd26afdcf223398c991
BezdomnyiBox <vova26012003@gmail.com>
2024-09-26 02:45:34 +1000

    created_db.php

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db = 'forum';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error connecting to the database: " . $e->getMessage());
+}
